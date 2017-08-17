3dd6f6acb1df7f1cd2eff8c0b9d3bd778b579fe9
Yannick de Lange <ydelange@hostnet.nl>
2017-08-17 13:59:47 +0200

    Fixed FileResolver test

--- test/Import/Nodejs/FileResolverTest.php
+++ test/Import/Nodejs/FileResolverTest.php
@@ -16,28 +16,28 @@ class FileResolverTest extends TestCase
 
     protected function setUp()
     {
-        $this->file_resolver = new FileResolver(['.js', '.json', '.node']);
+        $this->file_resolver = new FileResolver(__DIR__ . '/../../fixtures', ['.js', '.json', '.node']);
     }
 
     public function testAsRequireFile()
     {
-        $parent = new Module('bar/baz', new File(__DIR__ . '/../../fixtures/node_modules/bar/baz.js'));
+        $parent = new Module('bar/baz', new File('node_modules/bar/baz.js'));
 
         $import = $this->file_resolver->asRequire('./foo/hom', $parent);
 
-        self::assertInstanceOf(Module::class, $import->getImport());
-        self::assertSame('fixtures/node_modules/bar/foo/hom.js', $import->getImport()->getPath());
-        self::assertSame('bar/foo/hom', $import->getImport()->getName());
+        self::assertInstanceOf(Module::class, $import->getImportedFile());
+        self::assertSame('node_modules/bar/foo/hom.js', $import->getImportedFile()->getPath());
+        self::assertSame('bar/foo/hom', $import->getImportedFile()->getName());
     }
 
     public function testAsRequireAsDir()
     {
-        $parent = new Module('bar/baz', new File(__DIR__ . '/../../fixtures/node_modules/bar/baz.js'));
+        $parent = new Module('bar/baz', new File('node_modules/bar/baz.js'));
 
         $import = $this->file_resolver->asRequire('./foo/bar', $parent);
 
-        self::assertInstanceOf(Module::class, $import->getImport());
-        self::assertSame('fixtures/node_modules/bar/foo/bar/index.js', $import->getImport()->getPath());
-        self::assertSame('bar/foo/bar', $import->getImport()->getName());
+        self::assertInstanceOf(Module::class, $import->getImportedFile());
+        self::assertSame('node_modules/bar/foo/bar/index.js', $import->getImportedFile()->getPath());
+        self::assertSame('bar/foo/bar', $import->getImportedFile()->getName());
     }
 }
