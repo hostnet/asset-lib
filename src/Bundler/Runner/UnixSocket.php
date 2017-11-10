982c265c49269a8c5ade93343563d49ae4b8b2ec
Hidde Boomsma <hboomsma@hostnet.nl>
2017-11-10 11:10:13 +0100

    Socket hangup fix

--- src/Bundler/Runner/UnixSocket.php
+++ src/Bundler/Runner/UnixSocket.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 namespace Hostnet\Component\Resolver\Bundler\Runner;
 
 use Hostnet\Component\Resolver\Bundler\Runner\Exception\SocketException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 /**
  * Small abstraction around the socket functions of php
@@ -18,25 +20,32 @@ use Hostnet\Component\Resolver\Bundler\Runner\Exception\SocketException;
  */
 class UnixSocket
 {
+    private $logger;
     private $socket;
 
-    public function __construct()
+    public function __construct(LoggerInterface $logger = null)
     {
+        $this->logger = $logger ?? new NullLogger();
         $this->socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
+        socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, ['sec' => 3, 'usec' => 0]);
     }
 
     public function connect(string $address): void
     {
-        if (! @socket_connect($this->socket, $address)) {
+        if (!@socket_connect($this->socket, $address)) {
+            $this->logger->debug('[UnixSocket] Connection exception');
             throw new SocketException('Problem connecting to the socket');
         }
     }
 
     public function send(string $to_send, int $bytes): void
     {
-        if (! @socket_send($this->socket, $to_send, $bytes, 0)) {
+        $this->logger->debug('[UnixSocket] Sending ' . $bytes);
+        if (!@socket_send($this->socket, $to_send, $bytes, 0)) {
+            $this->logger->debug('[UnixSocket] Sending exception');
             throw new SocketException('Problem sending to the socket');
         }
+        $this->logger->debug('[UnixSocket] Sending done');
     }
 
     public function receive(int $bytes): string
@@ -46,9 +55,26 @@ class UnixSocket
         }
 
         $buffer = '';
-        if (false === @socket_recv($this->socket, $buffer, $bytes, MSG_WAITALL)) {
-            throw new SocketException('Problem receiving from the socket');
+        $length = 0;
+
+        while ($bytes > $length) {
+            $care = '';
+            $res  = @socket_recv($this->socket, $care, 1, MSG_PEEK);
+
+            $this->logger->debug('[UnixSocket] Reading ' . ($bytes - $length) . ' bytes');
+            $res = @socket_read($this->socket, $bytes - $length);
+            if ($res === false) {
+                $this->logger->debug(
+                    '[UnixSocket] Reading exception '
+                    . socket_last_error() . ' (' . socket_strerror(socket_last_error()) . ')'
+                );
+                throw new SocketException('Problem receiving from the socket');
+            }
+            $length += strlen($res);
+            $buffer .= $res;
         }
+
+        $this->logger->debug('[UnixSocket] Read');
 
         return $buffer;
     }
