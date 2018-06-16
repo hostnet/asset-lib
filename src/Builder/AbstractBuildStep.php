beadd462928629f77b54fcec7ccad35c6cf27785
Yannick de Lange <yannick.l.88@gmail.com>
2018-06-16 22:59:52 +0200

    Added additional documentation to build steps and writers

--- src/Builder/AbstractBuildStep.php
+++ src/Builder/AbstractBuildStep.php
@@ -6,13 +6,49 @@ declare(strict_types=1);
 
 namespace Hostnet\Component\Resolver\Builder;
 
+/**
+ * Each build step represent a processing action for either a file or a module. This can be converting typescript to
+ * javascript, uglification or anything other that needs to be done to a file before it can be outputted.
+ *
+ * There are two types of build steps: file and module. The distinction is based on the acceptedStates of the step.
+ * States which are in the FILE_OPERATIONS are file build steps and those in the MODULE_OPERATIONS are module build
+ * steps.
+ *
+ * Each step can have different accepting states, for instance: minification can happen for any module state since it
+ * doesn't really care about when it happens. However, there can always be only one resulting state. Moreover, the
+ * resulting state can never be "before" one of the accepting states, otherwise this would create an infinite build
+ * loop. Finally, each step must have an accepting file extension and a resulting one.
+ *
+ * There is also a build priority, this can be used to indicate a the order in which steps need to be performed if
+ * multiple build can be taken. For instance, you might want to perform some module transformation over minification.
+ * The lower the priority the later it will be executed and the higher the sooner.
+ */
 abstract class AbstractBuildStep
 {
+    /**
+     * State indicating the file has been read from disk. From here the file must be transpiled or otherwise
+     * transitioned into a ready state.
+     */
     public const FILE_READ = 0;
+
+    /**
+     * State indicating that the file has been transpiled and is ready to be further transitioned into a ready state.
+     */
     public const FILE_TRANSPILED = 1;
+
+    /**
+     * State indicating that the file is ready to be bundled into a module.
+     */
     public const FILE_READY = 2;
 
+    /**
+     * State indicating that the files for the module have been collected and concatenated into a single module.
+     */
     public const MODULES_COLLECTED = 3;
+
+    /**
+     * State indicating that the module is ready to be written to disk.
+     */
     public const MODULES_READY = 4;
 
     /**
--- src/Builder/AbstractWriter.php
+++ src/Builder/AbstractWriter.php
@@ -6,35 +6,34 @@ declare(strict_types=1);
 
 namespace Hostnet\Component\Resolver\Builder;
 
+/**
+ * Each writer will write the content of a module to disk. Having multiple writers means you can write the file in
+ * different formats. For instance, you might want to output a gziped version of a file if your web-server does not
+ * gzip this natively.
+ *
+ * Writers have an accepting extension. This can also be "*" in case the writer can accept all extensions.
+ * Additionally, a writer is not required to always output a file. (The generic writer will always output the file
+ * as-is)
+ */
 abstract class AbstractWriter
 {
     /**
-     * Return the extension this step accepts. This should include the dot. I.e., ".css" or ".js".
+     * Return the extension this writer accepts. This should include the dot. I.e., ".css" or ".js". If the writer
+     * accepts all extensions (or does it's own logic) it can also return "*".
      *
      * @return string
      */
     abstract public function acceptedExtension(): string;
 
     /**
-     * Return the javascript module this step uses.
+     * Return the javascript module this writer uses.
      *
      * @return string
      */
     abstract public function getJsModule(): string;
 
     /**
-     * The lower the priority (i.e. 0 to 50) the later in the build process it
-     * will be picked. The higher, the sooner. Default is 50.
-     *
-     * @return int
-     */
-    public function writePriority(): int
-    {
-        return 50;
-    }
-
-    /**
-     * Calculate a unique hash for this step.
+     * Calculate a unique hash for this writer.
      *
      * @return string
      */
@@ -44,7 +43,6 @@ abstract class AbstractWriter
             \get_class($this),
             $this->acceptedExtension(),
             $this->getJsModule(),
-            $this->writePriority(),
         ]);
     }
 }
