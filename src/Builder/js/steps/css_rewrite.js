a7adaca71b63a93114743bcb76ebf578ed36fe66
Yannick de Lange <ydelange@hostnet.nl>
2018-07-05 12:26:27 +0200

    Added Font rewrite plugin to also provider the fonts in a css file

---
+++ src/Builder/js/steps/css_rewrite.js
@@ -0,0 +1,24 @@
+let path = require('path');
+
+module.exports = function (file, config) {
+    let content = file.content.toString().replace(/@font-face\s*{([^}]+)}/g, function (match, content) {
+        let rewrittenContent = content.replace(/url\(('([^']+)'|"([^"]+)")\)/g, function (match, quoted, u1, u2) {
+            let originalFile = u1 || u2;
+
+            // If the font was absolute, leave it be since it must be included in the application itself.
+            if (path.isAbsolute(originalFile)) {
+                return match;
+            }
+
+            let cssPath = config.paths.out + 'fonts/' + path.basename(originalFile);
+            let fontFile = path.normalize(path.join(config.paths.root, path.dirname(file.name), originalFile));
+
+            file.addAdditionalFile(cssPath, [fontFile]);
+
+            return 'url(' + JSON.stringify(path.relative(config.paths.out, cssPath)) + ')';
+        });
+        return '@font-face {' + rewrittenContent + '}';
+    });
+
+    return file.update(Buffer.from(content));
+};
---
+++ test/Plugin/CssFontRewitePluginTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright 2018 Hostnet B.V.
+ */
+declare(strict_types=1);
+
+namespace Hostnet\Component\Resolver\Plugin;
+
+use Hostnet\Component\Resolver\Builder\AbstractBuildStep;
+use Hostnet\Component\Resolver\Builder\Step\CssFontRewriteStep;
+use Hostnet\Component\Resolver\Builder\Step\IdentityBuildStep;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+
+/**
+ * @covers \Hostnet\Component\Resolver\Plugin\CssFontRewitePlugin
+ */
+class CssFontRewitePluginTest extends TestCase
+{
+    public function testActivate()
+    {
+        $css_font_rewite_plugin = new CssFontRewitePlugin();
+
+        $plugin_api = $this->prophesize(PluginApi::class);
+        $plugin_api->addBuildStep(Argument::type(CssFontRewriteStep::class))->shouldBeCalled();
+        $plugin_api->addBuildStep(Argument::that(function (AbstractBuildStep $step) {
+            return $step instanceof IdentityBuildStep
+                && \in_array($step->acceptedExtension(), ['.otf', '.ttf', '.woff', '.woff2'], true);
+        }))->shouldBeCalled();
+
+        $css_font_rewite_plugin->activate($plugin_api->reveal());
+    }
+}
