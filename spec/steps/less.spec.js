9c330ca590b0f24dfaf620200c213ca66d3fc120
Yannick de Lange <ydelange@hostnet.nl>
2018-07-23 13:31:49 +0200

    Upgraded less to 3.8.0

--- spec/steps/less.spec.js
+++ spec/steps/less.spec.js
@@ -14,6 +14,19 @@ describe("less.js", function () {
         expect(result.content.toString()).toBe('body {\n  background: red;\n}\nbody a {\n  font-size: 12px;\n}\n');
     });
 
+    it('execute with JS', function () {
+        let step = require('../../src/Builder/js/steps/less');
+        let result = step(new builder.File(
+            'foo.less',
+            'foo.less',
+            Buffer.from("@foo: `42`; body { margin: '@{foo}px'; }")
+        ));
+
+        expect(result.name).toBe('foo.less');
+        expect(result.module).toBe('foo.less');
+        expect(result.content.toString()).toBe("body {\n  margin: '42px';\n}\n");
+    });
+
     it('on error', function () {
         let step = require('../../src/Builder/js/steps/less');
         try {
--- src/Builder/js/steps/less.js
+++ src/Builder/js/steps/less.js
@@ -5,7 +5,8 @@ module.exports = function (file) {
     less.render(file.content.toString(), {
         filename: file.name,
         syncImport: true,
-        relativeUrls: true
+        relativeUrls: true,
+        javascriptEnabled: true
     }, function (error, output) {
         if (null !== error) {
             throw error.message + ' in ' + error.filename + ' on line ' + error.line;
